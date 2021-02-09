ff798bb2a6c72a44fcda8ade1af8612055024133
Максим Сергеевич <krovorgen@gmail.com>
2021-02-09 21:32:14 +0300

    complete new template

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,86 @@
-const gulp = require('gulp')
-const browserSync = require('browser-sync')
-const sass = require('gulp-sass')
-const cleanCSS = require('gulp-clean-css')
-const autoprefixer = require('gulp-autoprefixer')
-const rename = require('gulp-rename')
-const imagemin = require('gulp-imagemin')
-const htmlmin = require('gulp-htmlmin')
+const { src, dest, watch, parallel, series } = require('gulp')
 
-gulp.task('server', () => {
-    browserSync({
+const scss = require('gulp-sass')
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create()
+const uglify = require('gulp-uglify-es').default
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const del = require('del')
+
+function browsersync() {
+    browserSync.init({
         server: {
-            baseDir: 'dist',
+            baseDir: 'app',
         },
     })
+}
 
-    gulp.watch('src/*.html').on('change', browserSync.reload)
-})
+function cleanDist() {
+    return del('dist')
+}
 
-gulp.task('styles', () => {
-    return gulp
-        .src('src/sass/**/*.+(scss|sass)')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(rename({ suffix: '.min', prefix: '' }))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('dist/css'))
+function images() {
+    return src('app/images/**/*')
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+                }),
+            ])
+        )
+        .pipe(dest('dist/images'))
+}
+
+function sctipts() {
+    return src(['app/js/main.js'])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
         .pipe(browserSync.stream())
-})
+}
 
-gulp.task('watch', () => {
-    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'))
-    gulp.watch('src/*.html').on('change', gulp.parallel('html'))
-    gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'))
-    gulp.watch('src/fonts/**/*').on('all', gulp.parallel('fonts'))
-    gulp.watch('src/mailer/**/*').on('all', gulp.parallel('mailer'))
-    gulp.watch('src/pages/**/*').on('all', gulp.parallel('pages'))
-    gulp.watch('src/icons/**/*').on('all', gulp.parallel('icons'))
-    gulp.watch('src/img/**/*').on('all', gulp.parallel('images'))
-})
-
-gulp.task('html', () => {
-    return gulp
-        .src('src/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('dist/'))
-})
-
-gulp.task('scripts', () => {
-    return gulp
-        .src('src/js/**/*.js')
-        .pipe(gulp.dest('dist/js'))
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
+        .pipe(
+            autoPrefixer({
+                overrideBrowserslist: ['last 10 version'],
+                grid: true,
+            })
+        )
+        .pipe(dest('app/css'))
         .pipe(browserSync.stream())
-})
+}
 
-gulp.task('fonts', () => {
-    return gulp
-        .src('src/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe(browserSync.stream())
-})
+function build() {
+    return src(
+        [
+            'app/css/style.min.css',
+            'app/fonts/**/*',
+            'app/js/main.min.js',
+            'app/*.html',
+        ],
+        { base: 'app' }
+    ).pipe(dest('dist'))
+}
 
-gulp.task('mailer', () => {
-    return gulp
-        .src('src/mailer/**/*')
-        .pipe(gulp.dest('dist/mailer'))
-        .pipe(browserSync.stream())
-})
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+    watch(['app/*.html']).on('change', browserSync.reload)
+    watch(['app/js/main.js', '!app/js/main.min.js'], sctipts)
+}
 
-gulp.task('pages', () => {
-    return gulp
-        .src('src/pages/**/*')
-        .pipe(gulp.dest('dist/pages'))
-        .pipe(browserSync.stream())
-})
+exports.styles = styles
+exports.watching = watching
+exports.browsersync = browsersync
+exports.sctipts = sctipts
+exports.images = images
+exports.cleanDist = cleanDist
 
-gulp.task('icons', () => {
-    return gulp
-        .src('src/icons/**/*')
-        .pipe(gulp.dest('dist/icons'))
-        .pipe(browserSync.stream())
-})
-
-gulp.task('images', () => {
-    return gulp
-        .src('src/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
-        .pipe(browserSync.stream())
-})
-
-gulp.task(
-    'default',
-    gulp.parallel(
-        'watch',
-        'server',
-        'styles',
-        'scripts',
-        'fonts',
-        'icons',
-        'html',
-        'images',
-        'mailer',
-        'pages'
-    )
-)
+exports.build = series(cleanDist, images, build)
+exports.default = parallel(styles, sctipts, browsersync, watching)
