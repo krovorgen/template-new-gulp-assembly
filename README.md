bf64edbbf119032558dcbf145244b3cde83f9474
Maksim Biarozka <maksim.biarozka@infourok.biz>
2021-04-25 17:25:20 +0300

    Complete 2.1 version

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-![alt text](https://github.com/krovorgen/gradient-border-cards/blob/master/app/images/preview.PNG)
+![alt text]()
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,23 @@
-const { src, dest, watch, parallel, series } = require('gulp')
+const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss = require('gulp-sass')
-const concat = require('gulp-concat')
-const browserSync = require('browser-sync').create()
-const uglify = require('gulp-uglify-es').default
-const autoprefixer = require('gulp-autoprefixer')
-const imagemin = require('gulp-imagemin')
-const del = require('del')
-const htmlmin = require('gulp-htmlmin')
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
 
 function browsersync() {
     browserSync.init({
         server: {
             baseDir: 'app',
         },
-    })
+    });
 }
 
 function cleanDist() {
-    return del('dist')
-}
-
-function images() {
-    return src('app/images/**/*')
-        .pipe(
-            imagemin([
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.mozjpeg({ quality: 75, progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 }),
-                imagemin.svgo({
-                    plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-                }),
-            ])
-        )
-        .pipe(dest('dist/images'))
+    return del('dist');
 }
 
 function sctipts() {
@@ -41,7 +25,7 @@ function sctipts() {
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
 }
 
 function styles() {
@@ -52,40 +36,37 @@ function styles() {
             autoprefixer({
                 overrideBrowserslist: ['last 10 version'],
                 grid: true,
-            })
+            }),
         )
         .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
 }
 
 function build() {
     return src(
-        [
-            'app/css/style.min.css',
-            'app/fonts/**/*',
-            'app/js/main.min.js',
-        ],
-        { base: 'app' }
-    ).pipe(dest('dist'))
+        ['app/css/style.min.css', 'app/fonts/**/*', 'app/js/main.min.js', 'app/images/**/*'],
+        {
+            base: 'app',
+        },
+    ).pipe(dest('dist'));
 }
 function htmls() {
     return src('app/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(dest('dist/'))
+        .pipe(dest('dist/'));
 }
 function watching() {
-    watch(['app/scss/**/*.scss'], styles)
-    watch(['app/*.html']).on('change', browserSync.reload)
-    watch(['app/js/main.js', '!app/js/main.min.js'], sctipts)
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/*.html']).on('change', browserSync.reload);
+    watch(['app/js/main.js', '!app/js/main.min.js'], sctipts);
 }
 
-exports.styles = styles
-exports.watching = watching
-exports.browsersync = browsersync
-exports.sctipts = sctipts
-exports.images = images
-exports.cleanDist = cleanDist
-exports.htmls = htmls
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.sctipts = sctipts;
+exports.cleanDist = cleanDist;
+exports.htmls = htmls;
 
-exports.build = series(cleanDist, images, htmls, build)
-exports.default = parallel(styles, sctipts, browsersync, watching)
+exports.build = series(cleanDist, htmls, build);
+exports.default = parallel(styles, sctipts, browsersync, watching);
