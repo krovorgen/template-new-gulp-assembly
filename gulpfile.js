804f65eadb79218c18d3a93f53ea2b165877a587
maksim <maksim.biarozka@infourok.biz>
2021-09-04 18:00:39 +0300

    Added mixins and variables

--- gulpfile.js
+++ gulpfile.js
@@ -10,69 +10,69 @@ const concat = require('gulp-concat');
 const fileInclude = require('gulp-file-include');
 
 const browsersync = () => {
-    browserSync.init({
-        server: {
-            baseDir: 'build/',
-        },
-        notify: false,
-    });
+  browserSync.init({
+    server: {
+      baseDir: 'build/',
+    },
+    notify: false,
+  });
 };
 
 const watching = () => {
-    watch(['app/style-map/*.html']).on('change', browserSync.reload);
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/**/*.html'], html);
-    watch(['app/js/index.js', '!app/js/index.min.js', '!app/js/index.min.js.map'], scripts);
+  watch(['app/style-map/*.html']).on('change', browserSync.reload);
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/**/*.html'], html);
+  watch(['app/js/index.js', '!app/js/index.min.js', '!app/js/index.min.js.map'], scripts);
 };
 
 const styles = () => {
-    return src('app/scss/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css'))
-        .pipe(
-            autoprefixer({
-                overrideBrowserslist: ['last 10 version'],
-                grid: true,
-            }),
-        )
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'))
-        .pipe(browserSync.stream());
+  return src('app/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true,
+      }),
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/css'))
+    .pipe(browserSync.stream());
 };
 
 const scripts = () => {
-    return src(['app/js/index.js'])
-        .pipe(sourcemaps.init())
-        .pipe(concat('index.min.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/js'))
-        .pipe(browserSync.stream());
+  return src(['app/js/index.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('index.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/js'))
+    .pipe(browserSync.stream());
 };
 
 const html = () => {
-    return src(['app/*.html', '!app/parts/**/*.html'])
-        .pipe(
-            fileInclude({
-                prefix: '@@',
-                basepath: '@file',
-            }),
-        )
-        .pipe(dest('./build'))
-        .pipe(browserSync.stream());
+  return src(['app/*.html', '!app/parts/**/*.html'])
+    .pipe(
+      fileInclude({
+        prefix: '@@',
+        basepath: '@file',
+      }),
+    )
+    .pipe(dest('./build'))
+    .pipe(browserSync.stream());
 };
 
 const htmlMin = () => {
-    return src('build/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(dest('build/'));
+  return src('build/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('build/'));
 };
 
 const toBuild = () => {
-    return src(['app/fonts/**/*', 'app/images/**/*', 'app/style-map/**/*'], {
-        base: 'app',
-    }).pipe(dest('build'));
+  return src(['app/fonts/**/*', 'app/images/**/*', 'app/style-map/**/*'], {
+    base: 'app',
+  }).pipe(dest('build'));
 };
 
 exports.styles = styles;
